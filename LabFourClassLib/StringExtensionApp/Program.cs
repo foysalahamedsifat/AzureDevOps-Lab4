060622acf74a1760f0b57f6a1680b052921d5258
Foysal Sifat <fsifat@my.centennialcollege.ca>
2025-04-05 02:52:49 -0400

    Completed

---
+++ LabFourClassLib/StringExtensionApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LabFourClassLib;
+
+class Program
+{
+    static void Main()
+    {
+        string test1 = "Hello";
+        string test2 = "world";
+
+        Console.WriteLine($"'{test1}' starts with an uppercase? {test1.StartsWithUpper()}");
+        Console.WriteLine($"'{test2}' starts with an uppercase? {test2.StartsWithUpper()}");
+    }
+}
